272b7953a5a8ab663d10287c1501933795fcae93
Eldovyn <farras.pramudita@gmail.com>
2024-12-24 09:59:31 +0700

    fix(style): fix css not load at layout folder

--- src/layout/Auth/Login.tsx
+++ src/layout/Auth/Login.tsx
@@ -13,25 +13,25 @@ const LoginForm = () => {
     return (
         <>
             <form action="">
-                    <div className="p-4 text-white">
-                        <div className="mb-5 relative">
-                            <MdEmail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                            <Input placeholder="email address" className="pl-10 w-full" type="text" />
-                        </div>
-                        <div className="mt-5 relative">
-                            <MdLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                            <Input
-                                placeholder="password"
-                                className="pl-10 pr-10 w-full"
-                                type={showPassword ? "text" : "password"}
-                            />
-                            <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" onClick={togglePasswordVisibility}>
-                                {showPassword ? <MdVisibilityOff /> : <MdVisibility />}
-                            </span>
-                        </div>
+                <div className="p-4 text-white">
+                    <div className="mb-5 relative">
+                        <MdEmail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                        <Input placeholder="email address" className="pl-10 w-full" type="text" />
                     </div>
-                </form>
-                <Button className="bg-blue-700 w-[93%] flex mx-auto rounded-md mt-5">Login</Button>
+                    <div className="mt-5 relative">
+                        <MdLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                        <Input
+                            placeholder="password"
+                            className="pl-10 pr-10 w-full"
+                            type={showPassword ? "text" : "password"}
+                        />
+                        <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" onClick={togglePasswordVisibility}>
+                            {showPassword ? <MdVisibilityOff /> : <MdVisibility />}
+                        </span>
+                    </div>
+                </div>
+            </form>
+            <Button className="bg-blue-700 w-[93%] flex mx-auto rounded-md mt-5">Login</Button>
         </>
     )
 }
