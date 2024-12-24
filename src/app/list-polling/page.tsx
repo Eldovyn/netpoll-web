63cbb1a0a3dc69780ca015ae4847181d360ac0d1
Eldovyn <farras.pramudita@gmail.com>
2024-12-24 19:59:33 +0700

    feat(polling): add page list polling

--- src/app/list-polling/page.tsx
+++ src/app/list-polling/page.tsx
@@ -1,9 +1,47 @@
+'use client'
 import NavBar from "@/components/ui/navbar"
+import Form from "@/layout/Form"
+import {
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
 
 const ListPolling = () => {
     return (
         <>
             <NavBar />
+            <section className="w-full min-h-screen bg-black flex-row pt-8 pb-5 items-center justify-center">
+                <div className="bg-black w-[90%] mx-auto rounded-md p-8">
+                    <h1 className="text-white text-start text-lg font-semibold pb-2">List Polling</h1>
+                    <hr className="border-gray-800" />
+                    <br />
+                    <Form category="search-poll" />
+                    <br />
+                    <Card className="rounded-sm">
+                        <CardHeader>
+                            <CardTitle>Card Title</CardTitle>
+                            <CardDescription>Card Description</CardDescription>
+                        </CardHeader>
+                    </Card>
+                    <br />
+                    <Card className="rounded-sm">
+                        <CardHeader>
+                            <CardTitle>Card Title</CardTitle>
+                            <CardDescription>Card Description</CardDescription>
+                        </CardHeader>
+                    </Card>
+                    <br />
+                    <Card className="rounded-sm">
+                        <CardHeader>
+                            <CardTitle>Card Title</CardTitle>
+                            <CardDescription>Card Description</CardDescription>
+                        </CardHeader>
+                    </Card>
+                </div>
+            </section>
         </>
     )
 }
