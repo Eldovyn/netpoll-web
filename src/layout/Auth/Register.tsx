97a2ce16a17103b0aa420f00a1b1fd8f8fcf2c37
Eldovyn <farras.pramudita@gmail.com>
2024-12-27 20:42:16 +0700

    style(register): remove confirm password

--- src/layout/Auth/Register.tsx
+++ src/layout/Auth/Register.tsx
@@ -6,14 +6,9 @@ import { Button } from "@/components/ui/button";
 
 const RegisterForm = () => {
     const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     const togglePasswordVisibility = () => {
         setShowPassword(prevState => !prevState);
-    };
-
-    const toggleConfirmPasswordVisibility = () => {
-        setShowConfirmPassword(prevState => !prevState);
     };
 
     return (
@@ -37,17 +32,6 @@ const RegisterForm = () => {
                         />
                         <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" onClick={togglePasswordVisibility}>
                             {showPassword ? <MdVisibilityOff /> : <MdVisibility />}
-                        </span>
-                    </div>
-                    <div className="mt-5 relative">
-                        <MdLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                        <Input
-                            placeholder="confirm password"
-                            className="pl-10 pr-10 w-full"
-                            type={showConfirmPassword ? "text" : "password"}
-                        />
-                        <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" onClick={toggleConfirmPasswordVisibility}>
-                            {showConfirmPassword ? <MdVisibilityOff /> : <MdVisibility />}
                         </span>
                     </div>
                 </div>
