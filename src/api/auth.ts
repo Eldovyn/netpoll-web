30e30b0ce324676e745746453c0e21700b136f0d
Eldovyn <farras.pramudita@gmail.com>
2024-12-28 10:29:01 +0700

    feat(api): remove api account verification page

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -24,25 +24,4 @@ const apiAccountActive = async (options: UserOptions) => {
     return response;
 };
 
-const apiAccountActivePage = async (token: string) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_NETPOLL_API}netpoll/account-active/verification?token=${token}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    return response;
-};
-
-const apiUserAccountActive = async (options: UserOptions) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_NETPOLL_API}netpoll/account-active/verification`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(options),
-    });
-    return response;
-};
-
-export { apiRegister, apiAccountActive, apiAccountActivePage, apiUserAccountActive };
+export { apiRegister, apiAccountActive };
